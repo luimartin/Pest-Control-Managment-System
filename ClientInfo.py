b73379434d1e1d976c9a2ab4fe5065d94a22d72f
Lui Martin <luiandreimartin07@gmail.com>
2023-12-29 16:58:19 +0800

    Overall changes

--- ClientInfo.py
+++ ClientInfo.py
@@ -1,58 +1,35 @@
-from Contract import Contract
-from Schedule import Schedule
+from Database import *
+from QuerySettings import *
 
 class ClientInfo:
-    def __init__(self, name, contact_num, loc, email):
-        self.id = None
-        
-        self.__name = name
-        self.__contact_num = contact_num
-        self.__loc = loc
-        self.__email = email
-        
-        self.client = []
-        self.client.append(self)
-        
-        self.schedule = None
-        self.contract_detail = None
-        self.assign_tech = []
+    def __init__(self):
+        pass
+    
+    def add_client_info(self, name, email, phone_num, address):
+        query = (
+            "insert into CLIENT(name, email, phone_num, address, status, void)"
+            "values (%s, %s, %s, %s, %s, %s,)"
+        )
+        data = (name, email, phone_num, address, 'New', 0)
+        handle_transaction(query, data)
 
-        self.status = None
-        self.isVoid = False 
-    
-    def add_contract_detail(self, prob, treatment, start_contract_period, end_contract_period, scope, num_unit, valuation):
-        self.contract_detail = Contract(prob, treatment, start_contract_period, end_contract_period, scope, num_unit, valuation)
+    def add_contract(self, ref_id, problem, service_type, start_date, end_date, square_meter, unit, price):
+        query = (
+            "insert into CONTRACT (client_id, problem, service_type, start_date, end_date, square_meter, unit, price)"
+            "values (%s, %s, %s, %s, %s, %s, %s, %s)"
+        )
+        data = (ref_id, problem, service_type, start_date, end_date, square_meter, unit, price)
+        handle_transaction(query, data)
 
-    def add_schedule(self, sched_type, start_date, end_date, time_in, time_out):
-        self.schedule = Schedule(sched_type, start_date, end_date, time_in, time_out)
-    
-    def edit_personal_info(self, type, value):
-        match type: 
-            case "name":
-                self.__name = value
-            case "contact number":
-                self.__contact_num = value
-            case "location":
-                self.__loc = value
-            case "time out":
-                self.__email = value
+    def add_schedule(self, ref_id, sched_type, start_date, end_date, time_in, time_out):
+        query = (
+            "insert into SCHEDULE (client_id, schedule_type, start_date, end_date, time_in, time_out)"
+            "values (%s, %s, %s, %s, %s, %s)"
+        )
+        data = (ref_id, sched_type, start_date, end_date, time_in, time_out)
+        handle_transaction(query, data)
 
-    def __str__(self):
-        return self.__name, self.__contact_num, self.__loc, self.__email
-
-    @property
-    def name(self):
-        return self.__name
-    
-    @property
-    def contact_num(self):
-        return self.__contact_num
-    
-    @property
-    def loc(self):
-        return self.__loc
-    
-    @property
-    def email(self):
-        return self.__email
-    
+    # Partial Only
+    def search(self, input):
+        query = "select * from CLIENT where name = {}".format("\'"+input+"\'")
+        handle_select(query)   
