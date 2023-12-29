5de508bf9ed6fada3db89bd213e7697f26d03431
Lui Martin <luiandreimartin07@gmail.com>
2023-12-29 16:36:19 +0800

    Added add_schedule()

--- test.py
+++ test.py
@@ -21,9 +21,17 @@ class Client:
         data = (ref_id, problem, service_type, start_date, end_date, square_meter, unit, price)
         handle_transaction(query, data)
 
+    def add_schedule(self, ref_id, sched_type, start_date, end_date, time_in, time_out):
+        query = (
+            "insert into SCHEDULE (client_id, schedule_type, start_date, end_date, time_in, time_out)"
+            "values (%s, %s, %s, %s, %s, %s)"
+        )
+        data = (ref_id, sched_type, start_date, end_date, time_in, time_out)
+        handle_transaction(query, data)
+
     def search(self):
         query = "select * from CLIENT"
         handle_select(query)
         
 c = Client()
-c.add_contract(1, "General Infestation", "Pest and Termite", "2022-12-31", "2023-01-02", 187.0, 7, 15000)
+c.add_schedule(1, "Posting", "2022-12-31", "2022-01-02",  "09:00:00", "17:30:00")
