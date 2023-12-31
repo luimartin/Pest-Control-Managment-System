49976a1b7590335e9c28232d87576a8a94ed19cf
Lui Martin <luiandreimartin07@gmail.com>
2023-12-31 16:04:45 +0800

    Major Changes

--- Inventory.py
+++ Inventory.py
@@ -1,79 +1,48 @@
+from Database import *
+from QuerySettings import *
+
 class Inventory:
-    def __init__(self, name, item_type, quant, exp_date, desc):
-        self.id = None
-        
-        self.__name = name
-        self.__item_type = item_type
-        self.__quant = quant
-        self.__exp_date = exp_date
-        self.__desc = desc
+    def __init__(self):
+        pass
 
-        self.inventory = []
-        self.inventory.append(self)
+    def add_item(self, name, item_type, quant, expir, desc):
+        query = (
+            "insert into INVENTORY (item_name, item_type, quantity, expiration, description, void)"
+            "values (%s, %s, %s, %s, %s, %s)"
+        )
+        data = (name, item_type, quant, expir, desc, 0)
+        handle_transaction(query, data)
 
-        self.isVoid = False
-        self.curr_index = 0
+    def deduct_item(self, inv_id, amount):
+        if self.isItemExist(inv_id):
+            query = "update INVENTORY set quantity = quantity - %s WHERE item_id = %s"
+            data = (amount, inv_id)
+            handle_transaction(query, data)
 
-    def get_item(self, name, amount):   
-        if self.item_exist(name): 
-            if self.inventory[self.curr_index].__quant >= amount:
-                self.inventory[self.curr_index].__quant -= amount
-                print(f'Item obtained: {name} with amount of {amount}')
-                return True
-        
-        print(f'Item failed to obtain')
-        return False
- 
-    def restock_item(self, name, amount):
-        if self.item_exist(name):
-            self.__quant += amount
-            print(f'Item added: to {name} with amount of {amount}')
-            return True
-
-        print(f'Item failed to add')
-        return False
-
-    def item_exist(self, name):
-        for i in range(len(self.inventory)):
-            if name == self.inventory[i].name:
-                self.curr_index = i
-                return True
-        
-        return False
-
-    def edit_item_info(self, type, value):
-        match type:
-            case "name":
-                self.__name = value
-            case "item type":
-                self.__item_type = value
-            case "quant":
-                self.__quant = value
-            case "exp_date":
-                self.__exp_date = value
-            case "description":
-                self.__desc = value
-
-    def __str__(self):
-        return self.__name, self.__item_type, self.__quant, self.__exp_date, self.__desc
-
-    @property
-    def name(self):
-        return self.__name
+        return None
     
-    @property
-    def item_type(self):
-        return self.__item_type
-    
-    @property
-    def quant(self):
-        return self.__quant
+    def stock_item(self, inv_id, amount):
+        if self.isItemExist(inv_id):
+            query = "update INVENTORY set quantity = quantity + %s WHERE item_id = %s"
+            data = (amount, inv_id)
+            handle_transaction(query, data)
+        
+        return None
 
-    @property
-    def exp_date(self):
-        return self.__exp_date
+    def edit_inv_info(self, inv_id, categ, new_input):
+        temp = "update INVENTORY set {} = ".format(categ) 
+        query = temp + "%s where item_id = %s"
+        data = (new_input, inv_id)
+        handle_transaction(query, data)
 
-    @property
-    def desc(self):
-        return self.__desc    
-    
+    def choose_category(self, item_type):
+        query = "select item_name, quantity, date_format(expiration, '%m/%d/%y'), \
+        description from INVENTORY where item_type = {}".format("\'"+item_type+"\'")
+        return handle_select(query)
+
+    def isItemExist(self, inv_id):
+        query = "select void from INVENTORY where item_id =  {}".format(inv_id)
+        output = handle_select(query)[0][0] # Retriev from List and Tuple
+
+        if not output: return True
+        return False 
