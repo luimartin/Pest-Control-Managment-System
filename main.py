7f7f9ecc1f26cd0fb143a781b5633754d99cee85
Deinielle Cruz <deinielle.cruz@gmail.com>
2024-06-08 03:56:27 +0800

    ef uped partially fixed

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from clientinfo import ClientInfo
-from technician import Technician
-from inventory import Inventory
 from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
 from GUI.LogInUI import Window
-from user import User
-import database
-import time
 
    
 
