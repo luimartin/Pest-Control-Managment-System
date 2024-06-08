582657799882d66b6596109e25d8d3e1771396b0
bowie <bowie>
2024-06-08 21:04:59 +0800

    monthly sale, monthly average sale, and sale trend and prediction added

--- sales.py
+++ sales.py
@@ -2,6 +2,9 @@ from database import *
 from query_settings import *
 
 from datetime import date
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+import matplotlib.pyplot as plt
 
 class Sales:
     def __init__(self):
@@ -21,10 +24,55 @@ class Sales:
             data = (valid_id, figure, sale_date)
             handle_transaction(query, data)
 
-    def avg_sale(self):
+    def monthly_total_sale(self):
         query = (
-            ""
+            "select date_format(sale_date, '%Y-%m'), sum(figure) "
+            "from SALES "
+            "group by date_format(sale_date, '%Y-%m') "
+            "order by date_format(sale_date, '%Y-%m')"
         )
+        return handle_select(query)
+    
+    def monthly_avg_total_sale(self):
+        query = (
+            "select year(sale_date), date_format(sale_date, '%M'), avg(figure) from SALES "
+            "group by year(sale_date), month(sale_date) "
+            "order by year(sale_date), month(sale_date)"
+        )
+        return handle_select(query)
+
+    def sale_trend(self):
+        data = self.monthly_total_sale()
+    
+        dates = [row[0] for row in data]
+        total = [row[1] for row in data]
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(dates, total, marker='o', linestyle='-')
+        plt.xlabel('Date')
+        plt.ylabel('Total Sale')
+        plt.title('Monthly Total Sale')
+        plt.xticks(rotation=45)
+        plt.grid(True)
+        
+        window_size = 3 
+        moving_avg = []
+        for i in range(len(total) - window_size + 1):
+            avg = sum(total[i:i+window_size]) / window_size
+            moving_avg.append(avg)
+        
+        last_date = datetime.strptime(dates[-1], '%Y-%m')  # Convert last date string to datetime
+        next_dates = [last_date + relativedelta(months=i) for i in range(1, 4)]
+        
+        last_value = total[-1]
+        prediction = [last_value] * 3
+        
+        plt.plot(dates[:-window_size+1], moving_avg, color='red', linestyle='--', label='Moving Average')
+        plt.plot([date.strftime('%Y-%m') for date in next_dates], prediction, color='green', linestyle='--', label='Predicted Total Sale')
+        plt.legend()
+        
+        plt.tight_layout()
+        plt.show()
 
     def paid_client(self, client_id):
         query = """
@@ -45,5 +93,6 @@ class Sales:
         query = temp + "where sale_id = {}".format(sale_id)
         return handle_select(query)[0][0]
 
-#s = Sales()
-#s.add_sale(1, 10000)
+s = Sales()
+#s.add_sale(1, 30000)
+print(s.sale_trend())
